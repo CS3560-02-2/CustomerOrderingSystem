b65d5b1fcea7e1ed29992ae9c496f3cbb57d68c6
rkdcksrn99 <77480692+rkdcksrn99@users.noreply.github.com>
2021-10-08 20:13:11 -0700

    Add files via upload

---
+++ 3560/src/food.java
@@ -0,0 +1,6 @@
+//This is food class, which we can also think as menu.
+public class food 
+{
+	//We will have 3d array that contains foodname, foodprice, and foodimage. We will try to call this menu object by outputting by nested for loops.
+	Array[][][] menu= new array[String foodname][float foodPrice][foodImage]
+}
