326033c2d1b8b477407ea97185b7769abbd28a5a
Ethan-M-123 <79026999+Ethan-M-123@users.noreply.github.com>
2021-11-10 20:52:36 -0800

    connected scenes
    it was hard :(, until it wasn't...

--- 3560/src/Menu.java
+++ 3560/src/Menu.java
@@ -1,16 +1,44 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class Menu {
-
+    private Scene scene;
+    private Stage stage;
+    
     @FXML
-    void btnToDrinkMenu(MouseEvent event) {
-        System.out.println("Drink Menu Clicked");
+    void backToMain(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    void btnToPizzaMenu(MouseEvent event) {
-        System.out.println("Food Menu Clicked");
+    void btnToDrinkMenu(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("DrinksPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void btnToPizzaMenu(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("PizzaAndSidesPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        
     }
 
 }
--- 3560/src/PizzaAndSidesPage.java
+++ 3560/src/PizzaAndSidesPage.java
@@ -1,3 +1,24 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
 public class PizzaAndSidesPage {
+    private Scene scene;
+    private Stage stage;
+
+    @FXML
+    void backToMenu(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }
