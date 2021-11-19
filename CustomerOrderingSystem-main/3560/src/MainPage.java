febdb0b204dcaccf3692addfe74f63b09c4bbb9f
Samsational-9001 <91928204+Samsational-9001@users.noreply.github.com>
2021-11-18 20:22:02 -0800

    The most recent fix
    Using MySQL not PopSQL

---
+++ CustomerOrderingSystem-main/3560/src/MainPage.java
@@ -0,0 +1,51 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+
+public class MainPage{
+    private Scene scene;
+    private Stage stage;
+    
+    @FXML
+    void goToLogInPage(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("LogInPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void goToMenu(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void gotToAccountPage(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("CustomerPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void goToAccountCreation(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("NewAccountPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+}
---
+++ CustomerOrderingSystem-main/3560/src/NewAccountPage.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+import com.mysql.cj.xdevapi.Table;
+public class NewAccountPage implements Initializable{
+        private Scene scene;
+        private Stage stage;
+        String test;
+
+
+        @Override
+        public void initialize(URL location, ResourceBundle resources){
+        
+
+        
+    }
+
+    @FXML
+    void backToMenu(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    
+    @FXML
+    void saveUserData(ActionEvent event) {
+
+    }
+    
+}
